113c948e06eac54e5a0fcd21bbcd0022ad9f7221
ishikawakota1997 <k-ishikawa@cmind-co.jp>
2020-11-23 01:01:13 +0900

    4-14 kadaitesyutu

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,11 +1,11 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin')
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'プロフィールのページ')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
