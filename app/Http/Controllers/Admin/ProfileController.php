e485f0bed519a2acfd5aed5ebe04f0c461cbafca
ishikawakota1997 <k-ishikawa@cmind-co.jp>
2020-11-23 16:32:49 +0900

    4-16 kadaiteisyutu

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,8 +12,20 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+
+              // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
         return redirect('admin/profile/create');
     }
 
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,18 +1,12 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.profile')
+@extends('layouts.admin')
+@section('title', 'プロフィールの新規作成')
 
-
-{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィールのページ')
-
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Myプロフィール</h2>
+                <h2>プロフィール新規作成</h2>
                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
-
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -21,28 +15,28 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">指名</label>
+                        <label class="col-md-2" for="name">名前</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="title">性別</label>
+                        <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
-                            <input type="radio" class="form-control" name="gender" value="{{ old('gender') }}">男性
-                            <input type="radio" class="form-control" name="gender" value="{{ old('gender') }}">女性
+                            <input type="radio" class="form-control" name="gender" value="{{ old('gender') }}"><p>男性</p>
+                            <input type="radio" class="form-control" name="gender" value="{{ old('gender') }}"><p>女性</p>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="body">趣味</label>
+                        <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('hobby') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <label class="col-md-2" for="introduction">自己紹介</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     <input type="submit" class="btn btn-primary" value="更新">
