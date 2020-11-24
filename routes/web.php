cd0bbf7ebcb0c7795b89e4cf7dc9fa3e4d8fc08b
ishikawakota1997 <k-ishikawa@cmind-co.jp>
2020-11-25 01:34:30 +0900

    4-18 kadaiteisyutu

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create'); # 追記
+    Route::post('news/create', 'Admin\NewsController@create'); 
     Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit'); 
+    Route::post('news/edit', 'Admin\NewsController@update'); 
+    Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::post('profile/create', 'Admin\ProfileController@create'); # 追記
+    Route::post('profile/create', 'Admin\ProfileController@create'); 
     Route::get('profile', 'Admin\ProfileController@index');
     Route::get('profile/edit', 'Admin\ProfileController@add');
-    Route::post('profile/edit', 'Admin\ProfileController@update'); # 追記
+    Route::post('profile/edit', 'Admin\ProfileController@update'); 
   });
 
 
