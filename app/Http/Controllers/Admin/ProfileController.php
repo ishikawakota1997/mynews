36fc646710520a50faa9f21e8c45c634aa6c6c66
ishikawakota1997 <k-ishikawa@cmind-co.jp>
2020-11-27 00:35:19 +0900

    4-12.14nosyuusei

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+
+// 以下を追記
+use App\History;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -15,40 +21,79 @@ class ProfileController extends Controller
     public function create(Request $request)
     {
 
-              // 以下を追記
-      // Varidationを行う
-      $this->validate($request, Profile::$rules);
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
 
-      $profile = new Profile;
-      $form = $request->all();
+        $profile = new Profile;
+        $form = $request->all();
 
-      // データベースに保存する
-      $profile->fill($form);
-      $profile->save();
+        // formに画像があれば、保存する
+        if ($form['image']) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+
+        unset($form['_token']);
+        unset($form['image']);
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
 
         return redirect('admin/profile/create');
     }
-    // 以下を追記
+
     public function index(Request $request)
     {
-      $cond_name = $request->cond_name;
-      if ($cond_name != '') {
-          // 検索されたら検索結果を取得する
-          $posts = Profile::where('name', $cond_name)->get();
-      } else {
-          // それ以外はすべてのプロフィールを取得する
-          $posts = Profile::all();
-      }
-      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            $posts = Profile::where('name', $cond_name)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
 
-    public function edit()
+
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+
+        return view('admin.news.edit', ['news_form' => $news]);
     }
 
-    public function update()
+
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        // Validationをかける
+        $this->validate($request, News::$rules);
+        // News Modelからデータを取得する
+        $news = News::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $news_form = $request->all();
+        unset($news_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $news->fill($news_form)->save();
+
+        // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+        return redirect('admin/news/');
+    }
+
+    // 以下を追記
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $news = News::find($request->id);
+        // 削除する
+        $news->delete();
+        return redirect('admin/news/');
     }
 }
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.admin')
-@section('title', 'プロフィールの新規作成')
+@extends('layouts.profile')
+@section('title', 'Myプロフィールの新規作成')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>MYプロフィール新規作成</h2>
                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
