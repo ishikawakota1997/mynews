b4f50990b087f2566844d173f48c23d95dcf98eb
ishikawakota1997 <k-ishikawa@cmind-co.jp>
2020-11-28 12:54:28 +0900

    4-18 kadaisaiteisyutu

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create'); 
     Route::get('profile', 'Admin\ProfileController@index');
-    Route::get('profile/edit', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update'); 
-    Route::get('profile/delete', 'Admin\NewsController@delete');
+    Route::get('profile/delete', 'Admin\ProfileController@delete');
   });
 
 
